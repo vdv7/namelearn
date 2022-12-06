497788d34b1be8fe8ede17ce688d60ee497a3e85
vdv7 <vdv7@users.noreply.github.com>
2022-12-06 18:30:23 -0500

    multi-folder

--- namelearn.js
+++ namelearn.js
@@ -14,15 +14,21 @@ Screenshot_20221206_100317.png
 Screenshot_20221206_095936.png
 Screenshot_20221206_100047.png
 Screenshot_20221206_100115.png`.split('\n'),
-    'CS195-002':[]
+    'CS195-002':`Screenshot_20221206_053302.png
+Screenshot_20221206_053340.png
+Screenshot_20221206_053402.png
+Screenshot_20221206_053433.png
+Screenshot_20221206_053447.png
+Screenshot_20221206_053536.png`.split('\n')
 };
 
-const ALL_FILES=[].concat(...Object.values(FILES));
+const ALL_FILES=Object.entries(FILES).flatMap(([folder,images])=>Object.values(images).map(image=>folder+'/'+image));
 
 
 const img=document.getElementById('img');
 const screen=document.getElementById('screen');
 const next=document.getElementById('next');
+const folders=document.getElementById('folders');
 
 
 const randInt=i=>Math.floor(Math.random()*i);
@@ -32,16 +38,29 @@ const randChoice=arr=>arr[randInt(arr.length)];
 var currentGroup='CS195-001';
 
 function nextImage(){
-    if(currentGroup){
+    if(currentGroup in FILES){
         img.src=`imgs/${currentGroup}/${randChoice(FILES[currentGroup])}`;
     }else{
-
+        img.src=`imgs/${randChoice(ALL_FILES)}`;
     }
+}
 
+function addFolder(folder){
+    var folderLink=folders.appendChild(document.createElement('a'));
+    folderLink.href='#';
+    folderLink.innerText=folder;
+    folderLink.addEventListener('click',e=>{
+        currentGroup=folder;
+        nextImage();
+    });
 }
 
 function main(){
     nextImage();
+    for(var folder in FILES){
+        addFolder(folder);
+    }
+    addFolder('All Images');
     screen.addEventListener('click',e=>{
         if(screen.className=='transparent'){
             screen.className='';
